e973c7c445ab86c438d820b57f481482179589c6
lisaeriksson <lisa.c.eriksson@gmail.com>
2017-11-02 11:32:12 +0100

    adding expiry date function, setting CONST

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,16 @@
 
 class Account
+
+  STANDARD_VALIDITY_YRS = 5
+
   attr_accessor: :pin_code, :account_status, :funds, :balance :exp_date, :account_owner
 
 def initialize
   @funds = 1000
-  @account_owner
+  @account_owner = account_owner
+  @pin_code = rand(1000..9999)
+end
+
+def set_expiry_date
+  Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -12,4 +12,9 @@ describe Account do
     expect(number_length).to eq 4
   end
 
+  it 'is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+  end
+
 end
